6ea30cea3b8ccbc217a3c4f145dc5a4b3fae0de9
wyh <wangyahui@sict.ac.cn>
2015-04-28 11:26:32 +0800

    add cp2ftp

---
+++ .bashOoO/cp2ftp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+# share file  my ftp 
+
+copy2ftp(){
+    if [ "$#" != "0" ];then
+        cp $1 $2 
+        sudo chown -R ftp:ftp $2
+    fi
+}
+copy2ftp  $@
+
